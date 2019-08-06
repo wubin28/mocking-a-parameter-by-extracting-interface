47479c164e04f7e1d4bac7e31197684fb33fa0de
benwu <wubinben@icloud.com>
2019-08-06 09:16:34 +0800

    make TransactionLog.saveTransaction() not supported

--- src/main/java/kata/taminglegacycode/tlc1/TransactionLog.java
+++ src/main/java/kata/taminglegacycode/tlc1/TransactionLog.java
@@ -2,6 +2,6 @@ package kata.taminglegacycode.tlc1;
 
 public class TransactionLog {
     public void saveTransaction(PaydayTransaction paydayTransaction) {
-
+        throw new java.lang.UnsupportedOperationException("TransactionLog.saveTransaction() not supported yet.");
     }
 }
